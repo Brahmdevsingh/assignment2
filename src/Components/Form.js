11401e317b0ff6dbbb340e170d40e6715598d565
Brahmdevsingh <bdsinghsidhi@gmail.com>
2021-07-19 17:15:26 +0530

    my first commit

---
+++ src/Components/Form.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import './CSS/todo.css'
+
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            subject: "",
+
+
+        }
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+
+    firsthandler = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    lasthandler = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    emailhandler = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+    passwordhandler = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    subjecthandler = (event) => {
+        this.setState({
+            gender: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.firstName} ${this.state.lastName} ${this.state.email} ${this.state.subject} Registered Successfully !!!!`)
+        console.log(this.state);
+        this.setState({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: '',
+            subject: "",
+        })
+     event.preventDefault()
+        
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <h1>USER | REGISTRATION</h1>
+                    <label>FirstName :</label> <input type="text" value={this.state.firstName} onChange={this.firsthandler} placeholder="FirstName..." /><br />
+                    <label>LastName :</label> <input type="text" value={this.state.lastName} onChange={this.lasthandler} placeholder="LastName..." /><br />
+                    <label>Email :   </label> <input type="text" value={this.state.email} onChange={this.emailhandler} placeholder="Email..." /><br />
+                    <label>Password :</label> <input type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password..." /><br />
+                    <label>Subject :</label><select onChange={this.subjecthandler} defaultValue="Select Subject">
+                        <option defaultValue>Select Subject</option>
+                        <option value="React">React</option>
+                        <option value="Data Mining">Data Mining</option>
+                    </select><br />
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Form
